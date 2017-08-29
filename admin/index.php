3b3820f981fa76dc9019503b0900ac3b7d5bc226
mambax7 <mambax7@gmail.com>
2017-08-29 06:15:25 -0400

    change include_once to require_once

--- admin/index.php
+++ admin/index.php
@@ -1,19 +1,28 @@
 <?php
-/**
- * Printliminator module
+/*
+ * You may not change or alter any portion of this comment or credits
+ * of supporting developers from this source code or any supporting source code
+ * which is considered copyrighted (c) material of the original comment or credit authors.
  *
- * @copyright           The XOOPS Project https://github.com/XoopsModules25x
- * @license             http://www.fsf.org/copyleft/gpl.html GNU public license
- * @package             Printliminator
- * @since               0.1.0
- * @author              aerograf <https://www.shmel.org>
- * @version             $Id: blocks_mytype.php 2017-06-06
- **/
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
 
-include_once __DIR__ . '/admin_header.php';
+/**
+ * @copyright    XOOPS Project https://xoops.org/
+ * @license      GNU GPL 2 or later (http://www.gnu.org/licenses/gpl-2.0.html)
+ * @package
+ * @since
+ * @author     XOOPS Development Team
+ */
+
+require_once __DIR__ . '/admin_header.php';
+// Display Admin header
 xoops_cp_header();
+
 $adminObject = \Xmf\Module\Admin::getInstance();
 $adminObject->displayNavigation(basename(__FILE__));
 $adminObject->displayIndex();
 
-include_once __DIR__ . '/admin_footer.php';
+require_once __DIR__ . '/admin_footer.php';
